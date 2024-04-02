86091e9867510f719e8285fa91acffe82d9a4e18
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-04-01 20:14:36 -0400

    Bring encoding speed in js

--- main.js
+++ main.js
@@ -242,7 +242,22 @@ document.getElementById("myForm").addEventListener("submit", function(event){
 
 document.getElementById("ekran").innerHTML = ""; // Ekranni tozalash
 
+function appendToResult(value) {
+  document.getElementById('result').value += value;
+}
 
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function calculate() {
+  var result = document.getElementById('result').value;
+  try {
+      document.getElementById('result').value = eval(result);
+  } catch (error) {
+      document.getElementById('result').value = 'Error';
+  }
+}
 
 
 
